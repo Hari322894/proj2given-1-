e51f869de6a05d6211c97dc4376be92c17e96e4f
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-17 01:17:28 -0800

    1

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,105 +1,264 @@
 #include "XMLWriter.h"
-#include <memory>
+
+
+#include <sstream>
+
+
 #include <unordered_map>
 
+
+
 struct CXMLWriter::SImplementation {
+
+
     std::shared_ptr<CDataSink> DataSink;
-    int IndentationLevel = 0;
-    bool PrettyPrint = true;
+
+
     
+
+
+    // Special character map for XML escaping
+
+
     const std::unordered_map<char, std::string> EscapeMap = {
+
+
         {'&', "&amp;"},
+
+
         {'"', "&quot;"},
+
+
         {'\'', "&apos;"},
+
+
         {'<', "&lt;"},
+
+
         {'>', "&gt;"}
+
+
     };
+
+
     
+
+
     SImplementation(std::shared_ptr<CDataSink> sink) : DataSink(std::move(sink)) {}
+
+
     
+
+
+    // Helper function to escape special XML characters
+
+
     std::string EscapeString(const std::string &str) {
+
+
         std::string result;
-        result.reserve(str.size() * 2);
+
+
+        result.reserve(str.size() * 2); // Reserve extra space for potential escapes
+
+
+        
+
+
         for (char c : str) {
+
+
             auto it = EscapeMap.find(c);
-            result += (it != EscapeMap.end()) ? it->second : std::string(1, c);
+
+
+            if (it != EscapeMap.end()) {
+
+
+                result += it->second;
+
+
+            } else {
+
+
+                result += c;
+
+
+            }
+
+
         }
+
+
+        
+
+
         return result;
+
+
     }
+
+
     
-    std::string GetIndentation() {
-        return PrettyPrint ? std::string(IndentationLevel, '\t') : "";
-    }
-    
+
+
     bool WriteEntity(const SXMLEntity &entity) {
+
+
         std::string output;
+
+
         
-        // Add newline before elements except for the first one
-        if (PrettyPrint && entity.DType != SXMLEntity::EType::CharData && IndentationLevel > 0) {
-            output += "\n";
-        }
-        
-        // Add indentation for all types except CharData
-        if (PrettyPrint && entity.DType != SXMLEntity::EType::CharData) {
-            output += GetIndentation();
-        }
-        
+
+
+        // Handle different entity types
+
+
         switch (entity.DType) {
+
+
             case SXMLEntity::EType::StartElement:
-                output += "<" + entity.DNameData;
+
+
+                output = "<" + entity.DNameData;
+
+
+                
+
+
+                // Add attributes
+
+
                 for (const auto &attr : entity.DAttributes) {
+
+
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
+
+
                 }
+
+
                 output += ">";
-                if (PrettyPrint) {
-                    IndentationLevel++;
-                }
+
+
                 break;
+
+
                 
+
+
             case SXMLEntity::EType::EndElement:
-                if (PrettyPrint) {
-                    IndentationLevel--;
-                }
-                output += "</" + entity.DNameData + ">";
+
+
+                output = "</" + entity.DNameData + ">";
+
+
                 break;
+
+
                 
+
+
             case SXMLEntity::EType::CharData:
-                output += EscapeString(entity.DNameData);
+
+
+                output = EscapeString(entity.DNameData);
+
+
                 break;
+
+
                 
+
+
             case SXMLEntity::EType::CompleteElement:
-                output += "<" + entity.DNameData;
+
+
+                output = "<" + entity.DNameData;
+
+
+                
+
+
+                // Add attributes
+
+
                 for (const auto &attr : entity.DAttributes) {
+
+
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
+
+
                 }
+
+
+                
+
+
+                // Self-closing tag
+
+
                 output += "/>";
+
+
                 break;
+
+
         }
+
+
         
-        // Add newline after elements except CharData and CompleteElement
-        if (PrettyPrint && 
-            entity.DType != SXMLEntity::EType::CharData && 
-            entity.DType != SXMLEntity::EType::CompleteElement) {
-            output += "\n";
-        }
-        
-        return DataSink->Write(std::vector<uint8_t>(output.begin(), output.end()));
+
+
+        // Write the output to the data sink
+
+
+        return DataSink->Write(std::vector<char>(output.begin(), output.end()));
+
+
     }
+
+
     
+
+
     bool Flush() {
-        return true;
+
+
+        return true; // No Flush() method in CDataSink
+
+
     }
+
+
 };
 
+
+
 CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
-    : DImplementation(std::make_unique<SImplementation>(std::move(sink))) {}
+
+
+    : DImplementation(std::make_unique<SImplementation>(sink)) {}
+
+
 
 CXMLWriter::~CXMLWriter() = default;
 
+
+
 bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
+
+
     return DImplementation->WriteEntity(entity);
+
+
 }
 
+
+
 bool CXMLWriter::Flush() {
+
+
     return DImplementation->Flush();
+
+
 }
