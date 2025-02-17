fc9213e5f0ad888f018096df71708c423eaa9ed0
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-16 18:18:01 -0800

    1

--- proj2/src/DSVWriter.cpp
+++ proj2/src/DSVWriter.cpp
@@ -9,19 +9,33 @@ struct CDSVWriter::SImplementation {
     SImplementation(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall)
         : Sink(sink), Delimiter(delimiter), QuoteAll(quoteall) {}
     
-    bool WriteRow(const std::vector<std::string>& row) {
-        for (size_t i = 0; i < row.size(); ++i) {
-            if (QuoteAll || row[i].find(Delimiter) != std::string::npos) {
-                Sink->Put('"');
-                for (char ch : row[i]) Sink->Put(ch);
-                Sink->Put('"');
-            } else {
-                for (char ch : row[i]) Sink->Put(ch);
+        bool WriteRow(const std::vector<std::string>& row) {
+            for (size_t i = 0; i < row.size(); ++i) {
+                bool needsQuotes = QuoteAll || row[i].find(Delimiter) != std::string::npos || row[i].find('"') != std::string::npos;
+        
+                if (needsQuotes) {
+                    Sink->Put('"');
+                    for (char ch : row[i]) {
+                        if (ch == '"') {
+                            Sink->Put('"'); // Escape quotes by doubling them
+                            Sink->Put('"');
+                        } else {
+                            Sink->Put(ch);
+                        }
+                    }
+                    Sink->Put('"');
+                } else {
+                    for (char ch : row[i]) {
+                        Sink->Put(ch);
+                    }
+                }
+        
+                if (i < row.size() - 1) {
+                    Sink->Put(Delimiter);
+                }
             }
-            if (i < row.size() - 1) Sink->Put(Delimiter);
+            return Sink->Put('\n');
         }
-        return Sink->Put('\n');
-    }
 };
 
 CDSVWriter::CDSVWriter(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall)
