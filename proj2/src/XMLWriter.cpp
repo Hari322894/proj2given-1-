e666663e1ed53c47abe1f12a816520991b5b8d25
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-17 02:27:56 -0800

    1

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,84 +1,74 @@
 #include "XMLWriter.h"
 #include <sstream>
-#include <unordered_map>
 
 struct CXMLWriter::SImplementation {
     std::shared_ptr<CDataSink> DataSink;
-    const std::unordered_map<char, std::string> EscapeMap = {
-        {'&', "&amp;"},
-        {'"', "&quot;"},
-        {'\'', "&apos;"},
-        {'<', "&lt;"},
-        {'>', "&gt;"}
-    };
-    int IndentationLevel = 0;
-    const std::string IndentationString = "\t";
-    bool IsPreviousStartElement = false;
 
     SImplementation(std::shared_ptr<CDataSink> sink) : DataSink(std::move(sink)) {}
 
+    // Helper function to escape special XML characters
     std::string EscapeString(const std::string &str) {
         std::string result;
         result.reserve(str.size() * 2); // Reserve extra space for potential escapes
+
         for (char c : str) {
-            auto it = EscapeMap.find(c);
-            if (it != EscapeMap.end()) {
-                result += it->second;
-            } else {
-                result += c;
+            switch (c) {
+                case '&':
+                    result += "&amp;";
+                    break;
+                case '"':
+                    result += "&quot;";
+                    break;
+                case '\'':
+                    result += "&apos;";
+                    break;
+                case '<':
+                    result += "&lt;";
+                    break;
+                case '>':
+                    result += "&gt;";
+                    break;
+                default:
+                    result += c; // Add the character as is
             }
         }
         return result;
     }
 
-    void AddIndentation(std::string &output) {
-        if (IsPreviousStartElement) {
-            output += "\n" + std::string(IndentationLevel, '\t');
-            IsPreviousStartElement = false;
-        }
-    }
-
     bool WriteEntity(const SXMLEntity &entity) {
         std::string output;
+        // Handle different entity types
         switch (entity.DType) {
             case SXMLEntity::EType::StartElement:
-                AddIndentation(output);
-                output += "<" + entity.DNameData;
+                output = "<" + entity.DNameData;
+                // Add attributes
                 for (const auto &attr : entity.DAttributes) {
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
                 }
                 output += ">";
-                IndentationLevel++;
-                IsPreviousStartElement = true;
                 break;
             case SXMLEntity::EType::EndElement:
-                IndentationLevel--;
-                AddIndentation(output);
-                output += "</" + entity.DNameData + ">";
-                IsPreviousStartElement = false; // Reset as we are closing an element
+                output = "</" + entity.DNameData + ">";
                 break;
             case SXMLEntity::EType::CharData:
-                // Avoid adding unnecessary newlines before character data
-                output += EscapeString(entity.DNameData);
+                output = EscapeString(entity.DNameData);
                 break;
             case SXMLEntity::EType::CompleteElement:
-                AddIndentation(output);
-                output += "<" + entity.DNameData;
+                output = "<" + entity.DNameData;
+                // Add attributes
                 for (const auto &attr : entity.DAttributes) {
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
                 }
+                // Self-closing tag
                 output += "/>";
-                IsPreviousStartElement = false; // Reset as this is a complete element
                 break;
         }
-
         // Write the output to the data sink
         return DataSink->Write(std::vector<char>(output.begin(), output.end()));
     }
 
     bool Flush() {
-        // No Flush() method in CDataSink
-        return true;
+        return true; // No Flush() method in CDataSink
     }
 };
 
