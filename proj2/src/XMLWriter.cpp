21f87663c8bfb8ee51a16985b4f36094e115a1c9
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-16 19:42:16 -0800

    1

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,97 +1,178 @@
 #include "XMLWriter.h"
+
 #include <sstream>
+
 #include <unordered_map>
 
+
 struct CXMLWriter::SImplementation {
+
     std::shared_ptr<CDataSink> DataSink;
+
     
+
     // Special character map for XML escaping
+
     const std::unordered_map<char, std::string> EscapeMap = {
+
         {'&', "&amp;"},
+
         {'"', "&quot;"},
+
         {'\'', "&apos;"},
+
         {'<', "&lt;"},
+
         {'>', "&gt;"}
+
     };
+
     
+
     SImplementation(std::shared_ptr<CDataSink> sink) : DataSink(std::move(sink)) {}
+
     
+
     // Helper function to escape special XML characters
+
     std::string EscapeString(const std::string &str) {
+
         std::string result;
-        result.reserve(str.size() * 2);
+
+        result.reserve(str.size() * 2); // Reserve extra space for potential escapes
+
         
+
         for (char c : str) {
+
             auto it = EscapeMap.find(c);
+
             if (it != EscapeMap.end()) {
+
                 result += it->second;
+
             } else {
+
                 result += c;
+
             }
+
         }
+
         
+
         return result;
+
     }
+
     
+
     bool WriteEntity(const SXMLEntity &entity) {
+
         std::string output;
+
         
+
+        // Handle different entity types
+
         switch (entity.DType) {
-            case SXMLEntity::EType::StartElement: {
+
+            case SXMLEntity::EType::StartElement:
+
                 output = "<" + entity.DNameData;
+
                 
+
                 // Add attributes
+
                 for (const auto &attr : entity.DAttributes) {
+
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
+
                 }
+
                 output += ">";
+
                 break;
-            }
-            
+
+                
+
             case SXMLEntity::EType::EndElement:
-                if (entity.DNameData == "osm") {
-                    // Don't write the closing osm tag
-                    return true;
-                }
+
                 output = "</" + entity.DNameData + ">";
+
                 break;
+
                 
+
             case SXMLEntity::EType::CharData:
-                // Now escape the character data content too
+
                 output = EscapeString(entity.DNameData);
+
                 break;
+
                 
-            case SXMLEntity::EType::CompleteElement: {
+
+            case SXMLEntity::EType::CompleteElement:
+
                 output = "<" + entity.DNameData;
+
                 
+
                 // Add attributes
+
                 for (const auto &attr : entity.DAttributes) {
+
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
+
                 }
+
+                
+
+                // Self-closing tag
+
                 output += "/>";
+
                 break;
-            }
+
         }
+
         
+
         // Write the output to the data sink
-        std::vector<char> outputChars(output.begin(), output.end());
-        return DataSink->Write(outputChars);
+
+        return DataSink->Write(std::vector<char>(output.begin(), output.end()));
+
     }
+
     
+
     bool Flush() {
-        return true;
+
+        return true; // No Flush() method in CDataSink
+
     }
+
 };
 
+
 CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
+
     : DImplementation(std::make_unique<SImplementation>(sink)) {}
+
 
 CXMLWriter::~CXMLWriter() = default;
 
+
 bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
+
     return DImplementation->WriteEntity(entity);
+
 }
 
+
 bool CXMLWriter::Flush() {
+
     return DImplementation->Flush();
+
 }
