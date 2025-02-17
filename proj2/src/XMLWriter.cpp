faa1ed2aa7f2b404c0776cf9a13aba9fea982383
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-17 09:16:16 -0800

    1

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -3,77 +3,79 @@
 
 struct CXMLWriter::SImplementation {
     std::shared_ptr<CDataSink> DataSink;
-    bool IsOSMStarted = false;
-    
-    SImplementation(std::shared_ptr<CDataSink> sink) : DataSink(std::move(sink)) {}
-    
+    int IndentLevel;
+
+    SImplementation(std::shared_ptr<CDataSink> sink) : DataSink(std::move(sink)), IndentLevel(0) {}
+
+    // Helper function to escape special XML characters
     std::string EscapeString(const std::string &str) {
         std::string result;
+        result.reserve(str.size() * 2); // Reserve extra space for potential escapes
+
         for (char c : str) {
             switch (c) {
-                case '&': result += "&amp;"; break;
-                case '"': result += "&quot;"; break;
-                case '\'': result += "&apos;"; break;
-                case '<': result += "&lt;"; break;
-                case '>': result += "&gt;"; break;
-                default: result += c;
+                case '&':
+                    result += "&amp;";
+                    break;
+                case '"':
+                    result += "&quot;";
+                    break;
+                case '\'':
+                    result += "&apos;";
+                    break;
+                case '<':
+                    result += "&lt;";
+                    break;
+                case '>':
+                    result += "&gt;";
+                    break;
+                default:
+                    result += c; // Add the character as is
             }
         }
         return result;
     }
 
+    std::string GetIndentation() const {
+        return std::string(IndentLevel * 4, ' '); // 4 spaces per indent level
+    }
+
     bool WriteEntity(const SXMLEntity &entity) {
         std::string output;
-        
+        // Handle different entity types
         switch (entity.DType) {
             case SXMLEntity::EType::StartElement:
-                output = "<" + entity.DNameData;
+                output = GetIndentation() + "<" + entity.DNameData;
+                // Add attributes
                 for (const auto &attr : entity.DAttributes) {
                     output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
                 }
-                if (entity.DNameData == "osm") {
-                    IsOSMStarted = true;
-                    output += ">\n\n\t";
-                } else {
-                    output += ">";
-                }
+                output += ">\n";
+                IndentLevel++;
                 break;
-                
             case SXMLEntity::EType::EndElement:
-                if (entity.DNameData == "osm") {
-                    output = "\n\n</osm>";
-                    IsOSMStarted = false;
-                } else {
-                    output = "</" + entity.DNameData + ">";
-                }
+                IndentLevel--;
+                output = GetIndentation() + "</" + entity.DNameData + ">\n";
                 break;
-                
             case SXMLEntity::EType::CharData:
-                output = EscapeString(entity.DNameData);
+                output = GetIndentation() + EscapeString(entity.DNameData) + "\n";
                 break;
-                
             case SXMLEntity::EType::CompleteElement:
-                if (IsOSMStarted && entity.DNameData == "node") {
-                    output = "\n\t\t<" + entity.DNameData;
-                    for (const auto &attr : entity.DAttributes) {
-                        output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
-                    }
-                    output += "/>\n\t";
-                } else {
-                    output = "<" + entity.DNameData;
-                    for (const auto &attr : entity.DAttributes) {
-                        output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
-                    }
-                    output += "/>";
+                output = GetIndentation() + "<" + entity.DNameData;
+                // Add attributes
+                for (const auto &attr : entity.DAttributes) {
+                    output += " " + attr.first + "=\"" + EscapeString(attr.second) + "\"";
                 }
+                // Self-closing tag
+                output += "/>\n";
                 break;
         }
-        
+        // Write the output to the data sink
         return DataSink->Write(std::vector<char>(output.begin(), output.end()));
     }
 
     bool Flush() {
-        return true;
+        return true; // No Flush() method in CDataSink
     }
 };
 
